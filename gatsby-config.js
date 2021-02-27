bc7392dcfe2bb4b5a2763e18ae7c4cf6fd557939
Margus Lehiste <margus@createcore.com>
2021-02-27 13:43:59 +0200

    vn icon in plugin config fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,10 +14,11 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/images/vnnpm install gatsby-plugin-offline.png`, // This path is relative to the root of the site.
+        icon: `src/images/vn.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
